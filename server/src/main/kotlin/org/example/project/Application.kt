7d98a064b65a4d301f0400821556b9a84940474a
Kito <76854495+AugustoMegener@users.noreply.github.com>
2025-10-25 13:46:49 -0300

    a

---
+++ server/src/main/kotlin/org/example/project/Application.kt
@@ -0,0 +1,23 @@
+package org.example.project
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticFiles
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            
+        }
+    }
+}
